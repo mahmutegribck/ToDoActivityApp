3a6c8fe03ecccb9900f1d68b44d6484c49e6aea5
mahmutegribck <104262911+mahmutegribck@users.noreply.github.com>
2023-09-18 19:43:51 +0300

    Data access layer created and dbcontext added

---
+++ ToDoActivityAppAPI/ToDoActivityAppAPI.DataAccess/ToDoActivityAppAPIDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoActivityAppAPI.Entity.Entities;
+
+namespace ToDoActivityAppAPI.DataAccess
+{
+    public class ToDoActivityAppAPIDbContext : DbContext
+    {
+        public ToDoActivityAppAPIDbContext(DbContextOptions<ToDoActivityAppAPIDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<ContactReply> ContactsReplies { get; set; }
+        
+        
+    }
+}
