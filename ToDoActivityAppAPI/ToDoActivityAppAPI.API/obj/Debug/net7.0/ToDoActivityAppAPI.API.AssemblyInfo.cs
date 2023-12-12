7d2da7954c552e0ccdc2311a6401100ad2b50cad
mahmutegribck <104262911+mahmutegribck@users.noreply.github.com>
2023-12-12 17:57:29 +0300

    Update gitignore

--- ToDoActivityAppAPI/ToDoActivityAppAPI.API/obj/Debug/net7.0/ToDoActivityAppAPI.API.AssemblyInfo.cs
+++ ToDoActivityAppAPI/ToDoActivityAppAPI.API/obj/Debug/net7.0/ToDoActivityAppAPI.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoActivityAppAPI.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3bb1d237082cc35255f4a2e78ee6ac70ced945")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoActivityAppAPI.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoActivityAppAPI.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ToDoActivityAppAPI/ToDoActivityAppAPI.DataAccess/obj/Debug/net7.0/ToDoActivityAppAPI.DataAccess.AssemblyInfo.cs
+++ ToDoActivityAppAPI/ToDoActivityAppAPI.DataAccess/obj/Debug/net7.0/ToDoActivityAppAPI.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoActivityAppAPI.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3bb1d237082cc35255f4a2e78ee6ac70ced945")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoActivityAppAPI.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoActivityAppAPI.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
